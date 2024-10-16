0d4c88b6cfcf9eb34423c497a10b3ed763f7e834
AlphaOwl <98154867+AlphaOwlGit@users.noreply.github.com>
2024-10-16 00:16:13 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,39 @@ app.append(header);
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 if (canvas) {
     const ctx = canvas.getContext("2d");
+    app.append(canvas);
     if (ctx) {
         ctx.fillStyle = "white";
         ctx.fillRect(0, 0, 256, 256);
+
+        let isDrawing = false;
+
+        canvas.addEventListener('mousedown', (event) => {
+            isDrawing = true;
+            ctx.beginPath();
+            ctx.moveTo(event.offsetX, event.offsetY);
+        });
+
+        canvas.addEventListener("mousemove", (event) => {
+            if (isDrawing) {
+                ctx.lineTo(event.offsetX, event.offsetY);
+                ctx.stroke();
+            }
+        });
+
+        canvas.addEventListener("mouseup", () => {
+            isDrawing = false;
+        });
+
+        canvas.addEventListener("mouseleave", () => {
+            isDrawing = false;
+        });
+
+        const clear = document.createElement("button");
+        clear.innerHTML = "Clear Drawing";
+        clear.addEventListener("click", () => {
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+        });
+        app.append(clear);
     }
 }
-app.append(canvas);
