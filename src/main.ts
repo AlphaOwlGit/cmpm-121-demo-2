f49c7892acd0578b765e8073245fc088c9ed1b03
AlphaOwl <98154867+AlphaOwlGit@users.noreply.github.com>
2024-10-16 02:22:56 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,9 @@ app.append(header);
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
 let isDrawing = false;
-const strokes: { x: number; y: number; } [][] = [];
+let strokes: { x: number; y: number; } [][] = [];
 let currentStroke: {x: number; y: number; }[] = [];
+let redoStack: { x: number; y: number; }[][] = [];
 
 const ctx = canvas.getContext("2d");
 app.append(canvas);
@@ -43,7 +44,6 @@ function redrawCanvas(ctx: CanvasRenderingContext2D) {
             });
         }
     });
-
     ctx.stroke();
 }
 
@@ -55,6 +55,7 @@ if (ctx) {
         isDrawing = true;
         currentStroke = [];
         strokes.push(currentStroke);
+        redoStack = [];
         addPointToStroke(event);
     });
 
@@ -74,15 +75,42 @@ if (ctx) {
         isDrawing = false;
     });
 
-    const clear = document.createElement("button");
-    clear.innerHTML = "Clear Drawing";
-    clear.addEventListener("click", () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-    });
-    app.append(clear);
-
     canvas.addEventListener('drawing-changed', () => {
         redrawCanvas(ctx);
     });
+
+    const clear = document.createElement("button");
+    clear.innerHTML = "Clear Drawing";
+    clear.addEventListener("click", () => {
+        strokes = [];
+        redoStack = [];
+        dispatchDrawingChangedEvent();
+    });
+    app.append(clear);
+
+    const undo = document.createElement("button");
+    undo.innerHTML = "Undo";
+    undo.addEventListener("click", () => {
+        if (strokes.length > 0) {
+            const lastStroke = strokes.pop();
+            if (lastStroke) {
+                redoStack.push(lastStroke);
+                dispatchDrawingChangedEvent();
+            }
+        }
+    });
+    app.append(undo);
+
+    const redo = document.createElement("button");
+    redo.innerHTML = "Redo";
+    redo.addEventListener("click", () => {
+        if (redoStack.length > 0) {
+            const stroke = redoStack.pop();
+            if (stroke) {
+                strokes.push(stroke);
+                dispatchDrawingChangedEvent();
+            }
+        }
+    });
+    app.appendChild(redo);
 }
